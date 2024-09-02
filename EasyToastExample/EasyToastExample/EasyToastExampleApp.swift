0b2a3b12a6fc895362aae1ef8a1dc7f54780a8b6
Banghua Zhao <banghua.zhao@gmail.com>
2024-09-02 17:56:34 +1200

    Add example project

---
+++ EasyToastExample/EasyToastExample/EasyToastExampleApp.swift
@@ -0,0 +1,16 @@
+//
+// Created by Banghua Zhao on 30/08/2024
+// Copyright Apps Bay Limited. All rights reserved.
+//
+  
+
+import SwiftUI
+
+@main
+struct EasyToastExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
