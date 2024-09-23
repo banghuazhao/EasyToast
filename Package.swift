86a8d073441a690e4ac4dc1133f1fcc8ef1d49b3
Banghua Zhao <banghua.zhao@gmail.com>
2024-09-23 21:36:00 +1200

    Refactor project structure

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EasyToast",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15), .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
